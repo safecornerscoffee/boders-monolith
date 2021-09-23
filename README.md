1f387ed483280fe8568ba03a0d1f53655e643a19
safecorners <safeconers.mobile@gmail.com>
2021-09-24 00:37:41 +0900

    test ApplicationContext should be loading

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # Borders Monolith
 
 [![Build Status](https://app.travis-ci.com/safecornerscoffee/borders-monolith.svg?branch=master)](https://app.travis-ci.com/safecornerscoffee/borders-monolith)
+[![Quality Gate Status](https://sonarcloud.io/api/project_badges/measure?project=safecornerscoffee_borders-monolith&metric=alert_status)](https://sonarcloud.io/dashboard?id=safecornerscoffee_borders-monolith)
+
 
--- src/test/java/com/safecornerscoffee/borders/BordersMonolithApplicationTests.java
+++ src/test/java/com/safecornerscoffee/borders/BordersMonolithApplicationTests.java
@@ -1,13 +1,21 @@
 package com.safecornerscoffee.borders;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
 class BordersMonolithApplicationTests {
 
+	@Autowired
+	ApplicationContext applicationContext;
+
 	@Test
 	void contextLoads() {
+		assertThat(applicationContext).isNotNull();
 	}
 
 }
