2f1af04569fe157f2b0b98a1940210cac8104968
safecorners <safeconers.mobile@gmail.com>
2021-09-23 22:52:47 +0900

    initialize project

---
+++ src/main/java/com/safecornerscoffee/borders/BordersMonolithApplication.java
@@ -0,0 +1,13 @@
+package com.safecornerscoffee.borders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BordersMonolithApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BordersMonolithApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/safecornerscoffee/borders/BordersMonolithApplicationTests.java
@@ -0,0 +1,13 @@
+package com.safecornerscoffee.borders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BordersMonolithApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
